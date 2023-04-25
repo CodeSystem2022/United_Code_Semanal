03af3ed3537b495a42e95927525d48afe320f6ea
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-04-25 15:44:33 -0300

    Python - 1.6 conexion hacia la bd (video 6)
    PYTHON - 1.6 Conexión hacia la base de datos en Python con el método fetchall() (video 6)

---
+++ Python/Semana 4/prueba_bd.py
@@ -0,0 +1,16 @@
+# Para conectarnos a Postgre
+import psycopg2
+
+conexion = psycopg2.connect(
+    user='postgres',
+    password='root',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+# print(conexion)
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia)  # De esta manera ejecutamos la sentencia
+registros = cursor.fetchall()  # Recuperamos todos los registros que seran una lista
