5e594a4d671a9bd33ea9f04118bc0c2ff587277f
Ferdegastaldi <99078237+Ferdegastaldi@users.noreply.github.com>
2023-05-03 14:48:18 -0300

    --- CLASE 04 JAVASCRIPT --- 1.5 Agregar y eliminar propiedades de los Objetos (video 6)

---
+++ Javascript/Semana4/01-04-objetos.js
@@ -0,0 +1,18 @@
+
+
+persona.apellida = 'Betancud'//Cambiamos dinamicamente el valor de un objeto
+delete persona.apellida;//Eliminamos el error
+console.log(persona);
+
+
+
+
+//Nro 3: la función Object.values(), regresa nuestro objeto como un arreglo
+console.log("Distintas formas de imprimir un objeto: forma 3");
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+//Nro 4: utilizaremos el método JSON.stringify
+console.log("Distintas formas de imprimir un objeto: forma 4");
+let personaString = JSON.stringify(persona); //convierte cada objeto en una cadena, asi se puede imprimir sin ningún problema
+console.log(personaString); 
