3179b7e79dd554c63f1953de066528a9a76b5e3f
cabreraangelgabriel <62511940+cabreraangelgabriel@users.noreply.github.com>
2023-05-07 22:25:59 -0300

    Create 01-04-objetos.js

---
+++ Javascript/Semana4/01-04-objetos.js
@@ -0,0 +1,21 @@
+let x = 10; // variable de tipo primitiva
+console.log(x.length);
+console.log("Tipos primitivos");
+
+//Objeto
+let persona = {
+    nombre: "Martina",
+    apellido: "Sauthier",
+    email: "martu_sauth@gmail.com",
+    edad: 19,
+    nombreCompleto: function(){ //METODO o funcion en javascript
+        return this.nombre+" "+this.apellido;
+    }
+} 
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto()); 
