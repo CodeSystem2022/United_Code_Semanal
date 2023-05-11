465e41ca627c24b70ecee9f14bfa74b5ed3487eb
Eugenia-olea <maruolea19@gmail.com>
2023-05-11 03:48:12 -0300

    Modificadores de acceso protected(video7)

--- Java/Semana4/Clase1.java
+++ Java/Semana4/Clase1.java
@@ -3,13 +3,21 @@ package paquete1;
 public class Clase1 {
 
     public String atributopublic = "Valor atributo publico";
+    
+    protected String atributoProtected = "Valor atributo protected";
+    
 
     public Clase1() {
         System.out.println("Constructor Public");
+    }
+    protected Clase1(String atributopublic){
+        System.out.println("Constructor protected");
     }
 
     public void metodoPublico() {
         System.out.println("Metodo Public");
     }
-
+    protected void metodoProtected(){
+        System.out.println("Metodo Protected");
+    }
 }
