af286aafdfff4b5c87d23fefff41a17546c2efec
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-05-16 14:57:10 -0300

    Clase 05 Python - 5.1 (video 1)
    5.1 Uso de with y psycopg2

---
+++ Python/Semana 6/prueba_bd.py
@@ -0,0 +1,21 @@
+# Para conectarnos a Postgre
+import psycopg2
+
+conexion = psycopg2.connect(user='postgres', password='root', host='127.0.0.1', port='5432', database='test_bd')
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona'
+            cursor.execute(sentencia)  # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchall()  # Recuperamos todos los registros que seran una lista
+            print(registros)
+
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+
+finally:
+    conexion.close()
+    
+
+# https://www.psycopg.org/docs/usage.html
