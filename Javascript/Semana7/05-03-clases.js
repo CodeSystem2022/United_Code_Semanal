5e84c89e96657b8379379d0bac76471c5c01f516
cabreraangelgabriel <62511940+cabreraangelgabriel@users.noreply.github.com>
2023-05-28 17:51:52 -0300

    JavaScript -- 6.3 hoisting y clases:
    Parte 1 y 2 (Video 5 y 6)

--- Javascript/Semana7/05-03-clases.js
+++ Javascript/Semana7/05-03-clases.js
@@ -1,38 +1,46 @@
+//let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined
+
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    } 
-
-
-    get nombre(){
-        return this._nombre;
-    }  
-
-    set nombre(nombre){
         this._nombre = nombre;
-    } 
-
-    get apellido(){
-        return this._apellido;
-    } 
-
-    set apellido(apellido){
         this._apellido = apellido;
     }
+    get nombre(){
+        return this._nombre
+    }
+    set nombre(nombre){
+        this._nombre= nombre;
+    }
+    get apellido(){
+        return this._apellido
+    }
+    set apellido(apellido){
+        this._apellido= apellido;
+    }
+
 }
 
-let persona1 = new Persona('Martín','Perez');
-console.log(persona1.nombre);
-persona1.nombre = 'Benicio';
+let persona1 = new Persona('Martin', 'Perez');
 console.log(persona1.nombre)
-persona1.apellido = 'Astrada';
-console.log(persona1.apellido);
+persona1.nombre='jopo'
+console.log(persona1.nombre);
 //console.log(persona1);
-let persona2 = new Persona('Carlos','Lara');
+
+//cambio de apellido TAREA
+console.log(persona1.apellido)
+persona1.apellido='Paulista'
+console.log(persona1.apellido);
+
+
+
+let persona2 = new Persona('Mario', 'Archundia');
+console.log(persona2.nombre)
+persona2.nombre = 'Maria Laura';
 console.log(persona2.nombre);
-persona2.nombre = 'Mia';
-console.log(persona2.nombre);
-persona2.apellido = 'Pereyra';
-console.log(persona2.apellido);
 //console.log(persona2);
+
+//cambio de apellido TAREA
+console.log(persona2.apellido)
+persona2.apellido='Pietro'
+console.log(persona2.apellido);
+//console.log(persona1);
