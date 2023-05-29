ae3419798acd654e49aa02276fc44c7d275cf6c4
Eugenia-olea <maruolea19@gmail.com>
2023-05-29 01:23:03 -0300

    Manejo de logging Parte 1(video 2 y 3)

--- Python/Semana8/logger_base.py
+++ Python/Semana8/logger_base.py
@@ -7,3 +7,10 @@ log.basicConfig(level=log.DEBUG,
                         log.FileHandler('capa_datos.log'),
                         log.StreamHandler()
                     ])
+if __name__ == '__main__':
+    log.basicConfig(level=log.DEBUG)
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel de warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
