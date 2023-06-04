8e0bf411b0ff3cead6f13785fe238733a4ad4961
Nicolás Aparicio <104036872+23nicolas@users.noreply.github.com>
2023-06-04 19:56:29 -0300

    Clase 08 JAVASCRIPT 8.1 Palabra static con métodos: Parte 1 y 2 (video 1)
    una vez que definimos una clase podemos agregar metodos que se asocien con nuestra clase, para esto utilizamos la palabra reservada static

--- Javascript/Semana7/05-03-clases.js
+++ Javascript/Semana7/05-03-clases.js
@@ -1,5 +1,4 @@
 //let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined
-
 class Persona{
     constructor(nombre, apellido){
         this._nombre = nombre;
@@ -20,73 +19,48 @@ class Persona{
     nombreCompleto(){
         return this._nombre + '  ' + this._apellido;;
     }
-
-
      //Sobreescribiendo el metodo de la clase padre (Object)
      toString(){ // regresa un string
         return this.nombreCompleto(); //se aplica el polimorfismo: multiples formas en tiempo de ejecucion
         // El metodo que se ejecuta depende si es una REFERENCIA de tipo padre o hija
     }
-    static saludar(){
-        console.log('Saludos desde este método static');
-    }
-    static saludar2(persona){
-        console.log(persona.nombre);
-    }
 
 }
 
-<<<<<<< Updated upstream
-let persona1 = new Persona('Martín','Perez');
-=======
 class Empleado extends Persona{ // Clase Hija
     constructor(nombre, apellido, departamento){
         super(nombre, apellido);
         this._departamento = departamento;
     }
-
     get departamento(){
         return this._departamento;
     }
-
     set departamento(departamento){
         this._departamento = departamento;
+    }
+
+    //Sobreescritura
+    nombreCompleto() {
+        return super.nombreCompleto() + ', ' + this._departamento;
     }
 }
 
 let persona1 = new Persona('Martin', 'Perez');
 console.log(persona1.nombre)
-persona1.nombre='jopo'
->>>>>>> Stashed changes
+persona1.nombre='Juan Carlos'
 console.log(persona1.nombre);
 //console.log(persona1);
 
-//cambio de apellido TAREA
-console.log(persona1.apellido)
-persona1.apellido='Paulista'
-console.log(persona1.apellido);
-//console.log(persona1);
-<<<<<<< Updated upstream
-let persona2 = new Persona('Carlos','Lara');
+let persona2 = new Persona('Carlos', 'Lara');
 console.log(persona2.nombre);
+persona1.nombre='María Laura';
+console.log(persona2.nombre); 
 //console.log(persona2);
 
-//cambio de apellido TAREA
-console.log(persona2.apellido)
-persona2.apellido='Pietro'
-console.log(persona2.apellido);
-//console.log(persona2);
-=======
-
-let empleado1 = new Empleado('Maria', 'Gomes', 'Sistemas');
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
 console.log(empleado1);
 console.log(empleado1.nombreCompleto());
 
-
-//Object.prototype.toString --> de esta manera accedemos a atributos y metodos de manera dinamica
+//Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinámica
 console.log(empleado1.toString());
-console.log(persona1.toString());
-//persona1.saludar();   no se utiliza desde el objeto, si se utiliza desde la clase
-Persona.saludar();
-Persona.saludar2(persona1);
->>>>>>> Stashed changes
+console.log(persona1.toString()); 
--- Javascript/Semana9/05-03-clases.js
+++ Javascript/Semana9/05-03-clases.js
@@ -1,105 +1,77 @@
-
-
-
-
-static contadorPersonas= 0;
-//email = 'Valor default email'; // atributo NO estatico 
-
-static get MAX_OBJ(){//Este metodo simula una constante
-    return 5;
-}
-
-
-
-
-
-
-constructor(nombre, apellido){ //metodo
-    this._nombre = nombre;
-    this._apellido = apellido; 
-    if(Persona.contadorPersonas < Persona.MAX_OBJ){
-        this.idPersona = ++Personas.contadorPersonas;
-    }else{
-        console.log('Se ha superado el maximo de objetos permitidos')
+//let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre
+    }
+    set nombre(nombre){
+        this._nombre= nombre;
+    }
+    get apellido(){
+        return this._apellido
+    }
+    set apellido(apellido){
+        this._apellido= apellido;
+    } 
+    nombreCompleto(){
+        return this._nombre + '  ' + this._apellido;;
+    }
+     //Sobreescribiendo el metodo de la clase padre (Object)
+     toString(){ // regresa un string
+        return this.nombreCompleto(); //se aplica el polimorfismo: multiples formas en tiempo de ejecucion
+        // El metodo que se ejecuta depende si es una REFERENCIA de tipo padre o hija
     }
 
+    static saludar(){
+        console.log('Saludos desde este método static');
+    }
 
+    static saludar2(persona){
+    console.log(persona.nombre);
+    }
+}
 
+class Empleado extends Persona{ // Clase Hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento;
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
 
+    //Sobreescritura
+    nombreCompleto() {
+        return super.nombreCompleto() + ', ' + this._departamento;
+    }
+}
 
+let persona1 = new Persona('Martin', 'Perez');
+console.log(persona1.nombre)
+persona1.nombre='Juan Carlos'
+console.log(persona1.nombre);
+//console.log(persona1);
 
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+persona1.nombre='María Laura';
+console.log(persona2.nombre);
+//console.log(persona2);
 
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
 
-    nombreCompleto(){ // metodo
-        return this.idPersona+' '+this._nombre+' '+this._apellido;
-    }  
-
-
-
-console.log(persona1.toString());
-console.log(persona2.toString());
+//Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinámica
 console.log(empleado1.toString());
-console.log(Persona.contadorPersonas);
+console.log(persona1.toString());
 
-console.log(Persona.MAX_OBJ);
-//Persona.MAX_OBJ = 10;//No se puede modificar
-console.log(Persona.MAX_OBJ);
-
-let persona4 = new Persona('Franco','Diaz');
-console.log(persona4.toString());
-
-let persona5 = new Persona('Liliana','Paz');
-console.log(persona5.toString);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//inicio video parte 6
-let persona3 = new Persona('Carla', 'Pertosi');
-console.log(persona3.toString());
-console.log(Persona.contadorPersonas);
+//persona1.saludar();   no se utiliza desde el objeto, si se utiliza desde la clase
+Persona.saludar();
+Persona.saludar2(persona1);
