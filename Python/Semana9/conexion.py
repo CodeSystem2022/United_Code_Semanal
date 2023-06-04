51196548c9f22ad4d84c0eceb1f78a90a4972ae2
Eugenia-olea <maruolea19@gmail.com>
2023-06-04 03:00:25 -0300

    Creación de la Clase Conexion (video5)

--- Python/Semana9/conexion.py
+++ Python/Semana9/conexion.py
@@ -6,3 +6,23 @@ class Conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+
+
+
+# Metodo de obtener cursor
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'se abrio correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'ocurrio un  error: {e}')
+                sys.exit()
+            else:
+                  return cls._cursor
+
+if __name__ == '__main__':
+        conexion.obtenerConexion()
+        conexion.obtenerCursor()   
