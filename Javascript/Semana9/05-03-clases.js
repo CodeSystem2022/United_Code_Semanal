21c29b9c0ae8ef44ad9495ae2f0cbbf22b33cff5
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-06-04 22:16:29 -0300

    Clase 08 JavaScript - 8.2 (video 3)
    Atributos estáticos

--- Javascript/Semana9/05-03-clases.js
+++ Javascript/Semana9/05-03-clases.js
@@ -1,24 +1,35 @@
 //let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined
 class Persona{
+
+static contadorObjetosPersona = 0;
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        Persona.contadorObjetosPersona ++;
+        console.log('Se incrementa el contador: ' + Persona.contadorObjetosPersona);
     }
+
     get nombre(){
         return this._nombre
     }
+
     set nombre(nombre){
         this._nombre= nombre;
     }
+
     get apellido(){
         return this._apellido
     }
+
     set apellido(apellido){
         this._apellido= apellido;
     } 
+
     nombreCompleto(){
         return this._nombre + '  ' + this._apellido;;
     }
+
      //Sobreescribiendo el metodo de la clase padre (Object)
      toString(){ // regresa un string
         return this.nombreCompleto(); //se aplica el polimorfismo: multiples formas en tiempo de ejecucion
@@ -39,9 +50,11 @@ class Empleado extends Persona{ // Clase Hija
         super(nombre, apellido);
         this._departamento = departamento;
     }
+
     get departamento(){
         return this._departamento;
     }
+
     set departamento(departamento){
         this._departamento = departamento;
     }
@@ -78,3 +91,7 @@ Persona.saludar2(persona1);
 
 Empleado.saludar();
 Empleado.saludar2(empleado1)
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
