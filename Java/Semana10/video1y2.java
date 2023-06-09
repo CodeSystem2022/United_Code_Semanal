c429190ca19b2e1552425143dfca6af7e25b3927
Katherinelop <69828273+Katherinelop@users.noreply.github.com>
2023-06-08 23:54:28 -0300

    video 1 y 2

---
+++ Java/Semana10/video1y2.java
@@ -0,0 +1,9 @@
+public class CalculadoraUTN{
+    public static void main(String[] args) {
+        System.out.println("****** Aplicación Calculadora ******");
+        var operando1 = 20;
+        var operando2 = 30;
+        var resultado = operando1 + operando2;
+        System.out.println("resultado = " + resultado);
+    }
+}
