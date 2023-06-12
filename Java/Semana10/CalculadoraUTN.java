873d5ec37ca1ca1f4c576484b9754a8b274f48f7
waltermoya <wmmoya@gmail.com>
2023-06-12 10:22:03 -0300

    Clase 9 Java - 9.6 Ciclo y su salida con break (video 6)

--- Java/Semana10/CalculadoraUTN.java
+++ Java/Semana10/CalculadoraUTN.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 
 public class CalculadoraUTN {
     public static void main(String[] args) {
+
+
+// Ingreso parte del video 6 -- Clase 9 Java - 9.6 Ciclo y su salida con break (video 6)
+//        while(true){ // Ciclo infinito
+//            System.out.println("******* Aplicacion Calculadora ***********");
+//            mostrarMenu();
+//            try {
+//                var operacion = Integer.parseInt(entrada.nextLine());
+//                if (operacion >= 1 && operacion <= 4) {
+//                    ejecutarOperacion(operacion,entrada);
+//                } // Fin del if
+//                else if (operacion == 5) {
+//                    System.out.println("Hasta pronto...");
+//                    break; // Rompe el ciclo y sale
+//                } else {
+//                    System.out.println("Opcion erronea " + operacion);
+//                }
+//                // Imprimimos un salto de linea antes de repetir el menu
+//                System.out.println();
+//            } catch (Exception e){ // Fin del try, comienzo del catch
+//                System.out.println("Ocurrio un error: "+e.getMessage());
+//                System.out.println();
+//            }
+//        } // Fin while
+
         /*linea 16: ejecutarOperacion(operacion, entrada)*/
         
         //Linea 41
@@ -27,7 +52,7 @@ public class CalculadoraUTN {
                 }
                 case 4 -> { //Division
                     resultado = operando1 / operando2;
-                    System.out.println("Resultado de la multiplicación: " + resultado);
+                    System.out.println("Resultado de la multiplicaciï¿½n: " + resultado);
                 }
                 default -> System.out.println("Opcion erronea: " + operacion);
             } //Fin Switch
