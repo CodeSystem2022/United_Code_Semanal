dc1418b6c994097d2e430e919b5249a9d876ad76
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-06-11 23:19:54 -0300

    Clase 9 Java - 9.9 (video 9 y 10)
    Función ejecutarOperacion y pruebas

---
+++ Java/Semana10/CalculadoraUTN.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        /*linea 16: ejecutarOperacion(operacion, entrada)*/
+        
+        //Linea 41
+        /*private static void ejecutarOperacion(int operacion, Scanner entrada) {
+            System.out.print("Digite el valor para el operando1: ");
+            var operando1 = Double.parseDouble(entrada.nextLine());
+            System.out.print("Digite el valor para el operando2: ");
+            var operando2 = Double.parseDouble(entrada.nextLine());
+            
+            double resultado;
+            switch(operacion) {
+                case 1 -> { //Suma
+                    resultado = operando1 + operando2;
+                    System.out.println("Resultado de la suma: " + resultado);
+                }
+                case 2 -> { //Resta
+                    resultado = operando1 - operando2;
+                    System.out.println("Resultado de la resta: " + resultado);
+                }
+                case 3 -> { //Multiplicacion 
+                    resultado = operando1 * operando2;
+                    System.out.println("Resultado de la division: " + resultado);
+                }
+                case 4 -> { //Division
+                    resultado = operando1 / operando2;
+                    System.out.println("Resultado de la multiplicación: " + resultado);
+                }
+                default -> System.out.println("Opcion erronea: " + operacion);
+            } //Fin Switch
+        } //Fin metodo ejecutarOperacion
+        */
+    }
+}
