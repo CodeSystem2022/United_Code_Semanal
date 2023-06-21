1517db481682413e017e43c756753fc7a5474535
cabreraangelgabriel <62511940+cabreraangelgabriel@users.noreply.github.com>
2023-06-20 23:29:33 -0300

    JAVA CLASE 10 VIDEOS 10 Y 11
    --- JAVA Clase 10 --- 10.4 Agregamos el try catch y el método ejecutarOperación() Incorporamos lambda y el método por referencia (video 11)#194

--- Java/Semana11/ListadopersonasApp.java
+++ Java/Semana11/ListadopersonasApp.java
@@ -1,36 +1,67 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class ListadoPersonasApp {
     public static void main(String[] args) {
-
+        Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        //Menú
+        var salir = false;
         while (!salir){
             mostrarMenu();
-            try {
+            try{
                 salir = ejecutarOperacion(entrada, personas);
             } catch (Exception e){
-                System.out.println("Ocurrió un error: "+e.getMessage());
+                System.out.println("Ocurrío un error: "+e.getMessage());
             }
             System.out.println();
-        } //Fin del ciclo while
-    } //Fin método main
+        }//Fin ciclo while
+    }//Fin método main
+    private static void mostrarMenu(){
+        //mostramos las opciones
+        System.out.print("""
+                ************ Listado de Personas*********
+                1. Agregar
+                2. Listar
+                3. Salir          
+                """);
+        System.out.print("Digite una de las opciones");
 
-    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
+    }//mostrar menu
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona>personas){
         var opcion = Integer.parseInt(entrada.nextLine());
         var salir = false;
-        //Revisamos la opcion digitada a través de un switch
+        //Revisamos la opcion a traves de un switch
         switch (opcion){
-            case 1 -> { //Agregar una persona a la lista
-                System.out.print("Digite el nombre: ");
+            case 1 -> {//Agregar una persona a la lista
+                System.out.println("Digite el nombre: ");
                 var nombre = entrada.nextLine();
-                System.out.print("Digite el teléfono: ");
+                System.out.println("DIgite el telefono: "  );
                 var tel = entrada.nextLine();
-                System.out.print("Digite el correo: ");
+                System.out.println("Digite el correo: ");
                 var email = entrada.nextLine();
-                // Creamos el objeto persona
+                //creamos el objeto persona
                 var persona = new Persona(nombre, tel, email);
-                //Agregamos la persona a la lista
+                //Agregamos persona a la lista
                 personas.add(persona);
-                System.out.println("La lista tiene: "+personas.size()+ " elementos");
-            } //Fin caso 1
+                System.out.println("La lista tiene: "+personas.size()+" Elementos");
+                 } // caso1
+            case 2 -> {//listar a las personas
+                System.out.println("Listado de personas: ");
+                // Mejoras con lambda y el método de referencia
+                //personas.forEach((persona ) -> System.out.print(persona));
+                  personas.forEach(System.out::println);
+            }
+            case 3 -> {//Salir
+                System.out.println("Hasta pronto...");
+                salir = true;
+            }//fin caso 3
+            default -> System.out.println("Opcion incorrecta "+ opcion);
+
+        }//Fin del switch
+        return salir;
+    }//fin ejecutar operación
+}
