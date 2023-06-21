d24f903721a5b3fcdecb7808e37e5e43d1190878
Katherinelop <69828273+Katherinelop@users.noreply.github.com>
2023-06-20 22:48:17 -0300

    ListadopersonasApp.java
     Agregamos el try catch y el método ejecutarOperación() Incorporamos lambda y el método por referencia.
    (video 8 y 9 )

--- Java/Semana11/ListadopersonasApp.java
+++ Java/Semana11/ListadopersonasApp.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Scanner;
 
 
 public class ListadoPersonasApp {
@@ -13,3 +15,22 @@ public class ListadoPersonasApp {
             System.out.println();
         } //Fin del ciclo while
     } //Fin método main
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        //Revisamos la opcion digitada a través de un switch
+        switch (opcion){
+            case 1 -> { //Agregar una persona a la lista
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el teléfono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo: ");
+                var email = entrada.nextLine();
+                // Creamos el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: "+personas.size()+ " elementos");
+            } //Fin caso 1
