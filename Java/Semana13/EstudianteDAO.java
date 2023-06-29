5a6240bc30f4c847f4fc16d2ffd3d818ce7772e7
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-06-29 18:25:27 -0300

    Clase 12 Java - 12.3 y 12.4 (video 6 y 7)
    12.3 Comenzamos con las pruebas del método listarEstudiantes() -> solucionamos algunos errores de conexión (video 6)
    12.4 Creamos en PersonaDAO() el método -> fin by id (buscarEstudiantePorId()) (video 7)

--- Java/Semana13/EstudianteDAO.java
+++ Java/Semana13/EstudianteDAO.java
@@ -4,13 +4,13 @@
 
 
 //Videos 4 y 5
-STRING sql = "SELECT * FROM estudiantes ORDER BY estudiantes2022";
+STRING sql = "SELECT * FROM estudiantes2022 ORDER BY idestudiantes2022";
 try{
     ps = con.prepareStatement(sql);
     rs = ps.executeQuery();
     while(rs.next()){
         var estudiante = new Estudiante();
-        estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+        estudiante.setIdEstudiante(rs.getInt("idestudiantes2022"));
         estudiante.setNombre(rs.getString("nombre"));
         estudiante.setApellido(rs.getString("apellido"));
         estudiante.setTelefono(rs.getString("telefono"));
@@ -30,6 +30,46 @@ finally{
 }//Fin finally
 return estudiantes;
 
+    //Video 7
+    //Método por id -> find by id
+    public boolean buscarEstudiantePorId(Estudiante estudiante) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022 WHERE idestudiantes2022=?"
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true; //Se encontro un registro
+            } //Fin if
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al buscar el estudiante: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error al cerrar la conexión: " + e.getMessage());
+            } //Fin catch
+        } //Fin finally
+        return false;
+    }
+
+    //Video 6
+    public static void main(String[] args) {
+        //Listar los estudiantes
+        var estudianteDao = new EstudianteDAO();
+        System.out.println("Listado de estudiantes");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println); //Función lambda para imprimir
+
+    }
 
 //Videos 9 y 10
 //Metodo agregar un nuevo estudiante
