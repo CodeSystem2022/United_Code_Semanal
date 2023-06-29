2837c908d8770216917f0c0fa0b0e44276bb2ce0
lucianocr21 <88411387+lucianocr21@users.noreply.github.com>
2023-06-29 00:05:43 -0300

    Add files via upload

---
+++ Javascript/Semana8/Clase 07--Javascript-7.2--sobreescritura.dart
@@ -0,0 +1,25 @@
+ // Sobreescritura
+     nombreCompleto() {
+         return super.nombreCompleto() + ' , ' + this.departamento;
+        
+         }
+      
+        }
+
+let Persona1 = new Persona ('Martin',  'Perez') ;
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+persona2.apellido = 'Perez'
+console.log(persona2.apellido);
+//console.log (persona1);
+let Persona 2 = new Persona ('Carlos', 'Lara');
+console.log (persona2.nombre);
+persona2.nombre = ('Maria Laura');
+console.log(persona2.nombre);
+persona2.apellido = ('Alvarez')
+console.log(persona2.apellido)
+//console.log (persona2);
+
+Let empleado1 new = Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log (empleado1) ;
