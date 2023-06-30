d3ae74422a2b208790fe22a5ac128bd9fb7a488a
Katherinelop <69828273+Katherinelop@users.noreply.github.com>
2023-06-29 22:02:30 -0300

    12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 1 y 2 (video 2 y 3)

--- Java/Semana13/EstudianteDAO.java
+++ Java/Semana13/EstudianteDAO.java
@@ -1,4 +1,22 @@
+package UTN.datos;
+//videos 2 y 3
+import UTN.dominio.Estudiante;
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class EstudianteDAO {
+    //Método listar
+    public List<Estudiante> listar(){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        //Creamos algunos objetos que son necesarios para comunicarnos con la base de datos
+        PreparedStatement ps; //Envía la sentencia a la base de datos
+        ResultSet rs; //Obtenemos el resultado de la base de datos
+        //Creamos un objeto de tipo conexion
+        Connection con = getConnection();
 
 
 
