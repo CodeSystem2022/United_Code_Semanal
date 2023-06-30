9dd2ca5872e02c665b0b90599b1a82cd886d01c9
Eugenia-olea <maruolea19@gmail.com>
2023-06-30 03:35:57 -0300

    prueba metodo: buscarEstudiantePorId

--- Java/Semana13/EstudianteDAO.java
+++ Java/Semana13/EstudianteDAO.java
@@ -87,6 +87,17 @@ return estudiantes;
         List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
         estudiantes.forEach(System.out::println); //Función lambda para imprimir
 
+        //Video 8
+        //Buscar por id
+        var estudiante1 = new Estudiante (1);
+        System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        var encontrado = estudianteDao.buscarEstudiantePorId (estudiante1);
+        if(encontrado)
+            System.out.println("Estudiante encontrado: "+estudiante1);
+        else
+            System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante());
+
+
     }
 
 //Videos 9 y 10
